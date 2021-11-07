cf273105dae59eb87b3c8847869953b62078444a
huynhbavinh <bavinh.congviec@gmail.com>
2021-11-07 19:30:50 +0700

    adding package for develop
    Vue-Animation : animxyz
    Vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -3,11 +3,23 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './routes.js'
 import storeConfig from './store'
-
 import Vuex from 'vuex'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueAnimXYZ from '@animxyz/vue'
+import '@animxyz/core' // Import css here if you haven't elsewhere
+
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 
 Vue.use(Vuex)
 Vue.use(VueRouter)
+Vue.use(VueAnimXYZ)
+
 
 const router = new VueRouter({
     mode: 'history',
